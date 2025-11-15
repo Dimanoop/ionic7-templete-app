e689f64f0c7dd07abfb64e85e947c1e959867179
ulybin05 <ulybin05@inbox.ru>
2025-11-15 08:28:30 +0000

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,12 +55,24 @@ const routes: Routes = [
   },
 
   {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+
+
+  
+  {
     
     path: 'forgot-password',
     loadChildren: () => import('./forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
     
     
   },
+
+{
+  path: 'marketplace',
+  loadChildren: () => import('./marketplace/marketplace.module').then(m => m.MarketplaceModule)
+}
 
   {
     path: '',
